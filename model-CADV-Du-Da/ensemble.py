51b58d9e43a7f563e24642d900aafce28b45c4c0
milkboylyf <liyifan0075@163.com>
2018-09-19 14:30:43 +0800

    add .gitignore
    .idea*
    *.pyc
    *.xml

--- model-CADV-Du-Da/ensemble.py
+++ model-CADV-Du-Da/ensemble.py
@@ -3,15 +3,15 @@ import pandas as pd
 try:
     df1 = pd.read_csv('model1_answer.txt')
 except:
-    print 'Somthing wrong. Maybe due to the version of sklearn or xgboost. ',
-    print 'please check the error message above'
+    print('Somthing wrong. Maybe due to the version of sklearn or xgboost. ')
+    print('please check the error message above')
     df1 = pd.read_csv('model1_answer_before.txt')
 
 try:
     df2 = pd.read_csv('model2_answer.txt')
 except:
-    print 'Somthing wrong. Maybe due to the version of sklearn or xgboost. ',
-    print 'please check the error message above'
+    print('Somthing wrong. Maybe due to the version of sklearn or xgboost. ')
+    print('please check the error message above')
     df2 = pd.read_csv('model2_answer_before.txt')
 
 df_final = df1.copy()
